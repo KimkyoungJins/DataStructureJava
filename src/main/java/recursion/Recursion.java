cdf9205fd583c87b1cc66bc48701674fada148f9
KimkyoungJins <kkjin722@gmail.com>
2024-06-17 23:23:42 +0900

    feat : 선택 정렬 메소드 수정

--- src/main/java/recursion/Recursion.java
+++ src/main/java/recursion/Recursion.java
@@ -48,40 +48,29 @@ public class Recursion {
         }
     }
 
+
     // 선택정렬 메소드
-    // 선택정렬이란
     public static void selectionSort(int[] arr) {
-        int n = arr.length;
-        // 배열의 끝까지 반복
+
+        int n = arr.length;         // 배열의 크기이다.
+
         for (int i = 0; i < n - 1; i++) {
-            // i 번째 위치에 들어갈 최소 원소의 위치를 찾는다.
+
+            // 초기값 잡아준다.
             int minIndex = i;
+
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if(arr[j] < arr[minIndex]){
                     minIndex = j;
                 }
             }
-            // 최소 원소를 i 번째 위치로 옮긴다.
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[i];
-            arr[i] = temp;
-        }
-    }
 
-    public static void selectionSrot(int[] arr) {
-        int n = arr.length;         // 배열의 길이이다.
-
-        for (int i = 0; i < n - 1; i++) {
-            // i번 째 위치에 들어갈 최소 원소의 위치를 찾는다.
-            int minIndex = i;
-
-
-
+            // 배열의 첫 번재 원소와 가장 작은 원소의 위치를 바꾸어 준다.
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
         }
 
-        // 최소 원소를 i 번재 위치로 옮기는 것이다.
-        int temp = arr[minIndex]; // 최소 원소를 i 번째 위치로 옮긴다.
-        arr[minIndex] = arr[i];
-        arr[int] = temp;
     }
+
 }
