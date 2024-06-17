89d481aabdbb5d81fb230d7a9127f2bf648c0458
KimkyoungJins <kkjin722@gmail.com>
2024-06-17 18:04:35 +0900

    feat: 추가 메소드

--- src/main/java/recursion/Recursion.java
+++ src/main/java/recursion/Recursion.java
@@ -5,7 +5,7 @@ public class Recursion {
     // 등차수열에 관한 함수인 것인가?
     // n은 n번째 항이며 difference는 공차이다.
     // 두개의 매개변수를 받는다.
-    public static int arithmeticSequence (int n, int difference) {
+    public static int arithmeticSequence(int n, int difference) {
 
         if (n == 1) {  // 첫 항을 설정해 준다.
             return 3;
@@ -16,7 +16,32 @@ public class Recursion {
 
     }
 
+    // 피보나치 수열 재귀함수로 나타낸다면??
+    // n번째 항의 숫자를 구하는 것이다.
+    public int fibonacci(int n) {
 
+        // base case
+        if (n <= 0) { // 예외처리를 해야한다.
+            throw new IllegalArgumentException("n must be positive integer");
 
+            // recursive case
+        } else if (n == 1 || n == 2) {
+            return 1;
+
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2 );
+        }
+    }
+
+    // 하노이 타워
+    public int hanoitower(int n, char formRod, char toRod, char endRod) {
+
+        if (n == 1) {
+            System.out.println("Only on disk is meaningless");
+        }
+
+        
+
+    }
 
 }
